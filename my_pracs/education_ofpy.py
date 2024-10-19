284545a1e7d3e4990d9093ff2b5ec668015f87bf
Oleksii Khodus <exalchel@gmail.com>
2024-10-20 00:50:10 +0300

    modified code

--- my_pracs/education_ofpy.py
+++ my_pracs/education_ofpy.py
@@ -16,38 +16,39 @@
                                                                       
                                                     
 # Chapter ^1
-
-def nameNmail():
+import math
+import time
+# Exercise ^1
+def task1():
     print("Oleksii Khodus")
     print("hello@yahoo.com")
-nameNmail()
 
-
-def greeting(name):
+# Exercise ^2
+def task2():
+    name = input("Enter your name please: ")
     print(f"Nice to meet you {name}")
-greeting(input("Enter your name please: "))
 
-
-def calc_area(leng, wid):
+# Exercise ^3
+def task3():
+    leng = float(input("Enter a length please (m): "))
+    wid = float(input("Enter a width please (m): "))
     calc = leng * wid
     print(f"Your area is {int(calc)} (m^2)")
 
-calc_area(float(input("Enter a length please (m): ")), 
-float(input("Enter a width please (m): ")))
-
-def calc_acres(leng, wid):
+# Exercise ^4
+def task4():
+    leng = float(input("Enter please length of garden (ft): "))
+    wid = float(input("Enter please width of garden (ft): "))
     acre = 43560
     calc = leng * wid
 
     acre_area = calc / acre
     print(f"\nYour area is {acre_area} (ac)")
 
-calc_acres(float(input("Enter please length of garden (ft): ")),
-float(input("Enter please width of garden (ft): ")))
-
-
-def bottles(liter, great_liter):
-
+# Exercise ^5
+def task5():
+    liter = int(input("Enter amount of bottle are less or equal to 1 (liter): "))
+    great_liter = int(input("Enter amount of bottles are greater than 1 (liter): "))
     calc_less = liter * 0.1
     print(f"\nThe amount of bottles are: {liter}\nYou've received: $ {calc_less:.2f}")
     
@@ -58,28 +59,30 @@ def bottles(liter, great_liter):
     summary_received = sum((calc_less, calc_great))
     print(f"\nYour summary of bottles are: {summary_bottles}\nAll your summary of received is: $ {summary_received:.2f}")
 
-bottles(int(input("Enter amount of bottle are less or equal to 1 (liter): ")),
-int(input("Enter amount of bottles are greater than 1 (liter): ")))
-
-
-def tips(sum_of_order):
+# Exercise ^6
+def task6():
+    sum_of_order = float(input("Enter please a summary of your order (dollars): "))
     tax = sum_of_order * 0.2 
     # base tax in Estonia is 20 %
     tips = sum_of_order * 0.18
 
     summary = sum((sum_of_order, tax, tips)) 
 
-    print(f"\nTax (20%) for this order is: ${tax:.2f}\nTips (18%) for this order is: ${tips:.2f}\nSummary of all of this: ${summary:.2f}")
+    print(f"\nTax (20%) for this order is: ${tax:.2f} \
+    \nTips (18%) for this order is: ${tips:.2f} \
+    \nSummary of all of this: ${summary:.2f}")
 
-tips(float(input("Enter please a summary of your order (dollars): ")))
-
-def summaryN(n):
+# Exercise ^7
+def task7():
+    n = int(input("Enter your number: "))
     # summary = (n * (n + 1)) / 2
     summary = sum(range(1, n + 1))
     print(int(summary))
-summaryN(int(input("Enter your number: ")))
 
-def market(souvenirs, baubles):
+# Exercise ^8
+def task8():
+    souvenirs = int(input("Amount of souvenirs: "))
+    baubles = int(input("Amount of baubles: "))
     souvenirs *= 75
     baubles *= 112
     print(f"\nYour weight of souvenirs is: {souvenirs} grammes")
@@ -88,9 +91,9 @@ def market(souvenirs, baubles):
     summary = sum((souvenirs, baubles))
     print(f"\nSummary of all packages is: {summary} grammes")
 
-market(int(input("Amount of souvenirs: ")), int(input("Amount of baubles: ")))
-
-def deposit(dep):
+# Exercise ^9
+def task9():
+    dep = float(input("Enter your amount of deposit (dollars): $ "))
     # percent is 4%
     first_year = dep * 0.04
     dep1 = dep + first_year
@@ -108,12 +111,10 @@ def deposit(dep):
     total = sum((first_year, second_year, third_year))
     print(f"\nTotal sum of percent of deposit is: $ {total:.2f}")
 
-
-deposit(float(input("Enter your amount of deposit (dollars): $ ")))
-
-import math 
-
-def arith(a, b):
+# Exercise ^10
+def task10():
+    a = int(input("Enter your first number: "))
+    b = int(input("Enter your second number: "))
     sumof = a + b
     minused = a - b
     multiply = a * b
@@ -128,17 +129,14 @@ def arith(a, b):
     print(f"\n Log10 of a is: {log10}")
     print(f"\n Square of a in b is: {sqrtB}")
 
-arith(int(input("Enter your first number: ")), int(input("Enter your second number: ")))
-
-def converter_fuel(fuel):
+# Exercise ^11
+def task11():
+    fuel = float(input("Enter how much MPG (miles-per-gallon) fuel your car uses: "))
     L100km = (282.48 / fuel)
     print(f"In Canada you'll have {L100km:.2f} L/100 km.") 
 
-converter_fuel(float(input("Enter how much MPG (miles-per-gallon) fuel your car uses: ")))
-
-import math
-
-def distance_btw():
+# Exercise ^12
+def task12():
     str_lat = "Enter a coordinate for the {} object latitude ({}): "
     str_long = "Enter a coordinate for the {} object longitude ({}): "
 
@@ -158,9 +156,9 @@ def distance_btw():
     )
     print(f"\nMost short distance between these two coordinates is: {dist:.2f} km")
 
-distance_btw()
-
-def change(cents):
+# Exercise ^13
+def task13():
+    cents = int(input("Enter a value of cents: ¢ "))
     bucks_2 = 200
     buck = 100
     cents_25 = 25
@@ -186,39 +184,42 @@ def change(cents):
     print(f"\nYou need {cents // cent_1} cent(s), with nominale of: ¢ {cent_1} cent")
     cents %= cent_1
 
-change(int(input("Enter a value of cents: ¢ ")))
+# Exercise ^14
+def task14():
+    ft = int(input("How much ft` in your height: "))
+    inches = int(input("How much inches`` in your height: "))
 
-
-def height(ft, inches):
     foot_in_inch = 12
     inch_in_cm = 2.54
 
     calc_of = (ft * foot_in_inch + inches) * inch_in_cm
     print(f"Your height in system [SI] will be {round(calc_of)} (cm)")
 
-height(int(input("How much ft` in your height: ")),int(input("How much inches`` in your height: ")))
-
-
-def distance_in_america(fts):
+# Exercise ^15
+def task15():
+    fts = int(input("Your distance in fts`: "))
     inches = fts * 12
     yd = fts * 0.33333
     mile = fts * 0.00018939
 
-    print(f"\nDistance in miles will be {mile:.2f}.\nDistance in yards will be {yd:.2f}.\nDistance in inches will be {inches}.")
+    print(f"\nDistance in miles will be {mile:.2f}. \
+    \nDistance in yards will be {yd:.2f}. \
+    \nDistance in inches will be {inches}.")
 
-distance_in_america(int(input("Your distance in fts`: ")))
-
-import math
-
-def radius(r):
+# Exercise ^16
+def task16():
+    r = int(input("Enter your radius (rad): "))
     area_circle = math.pi * (r ** 2)
     volume_sphere = (4/3) * math.pi * math.pow(r, 3)
-    print(f"\nYour area of the circle will be: {area_circle:.2f} (m2).\nYour volume of the sphere will be: {volume_sphere:.2f} (m3).")
+    print(f"\nYour area of the circle will be: {area_circle:.2f} (m2). \
+    \nYour volume of the sphere will be: {volume_sphere:.2f} (m3).")
 
-radius(int(input("Enter your radius (rad): ")))
+# Exercise ^17
+def task17():
+    water = int(input("Enter the mass of water (grammes): "))
+    tinit = float(input("Enter initial temperature (°C): "))
+    tend = float(input("Enter temperature at the end (°C): "))
 
-
-def heat_capacity(water, tinit, tend):
     c_water = 4.18
     absolute_t = tend - tinit
     q = water * c_water * absolute_t
@@ -231,101 +232,86 @@ def heat_capacity(water, tinit, tend):
 
     print(f"\nPrice for one cup of tea will be: $ {price:.2f}")
 
-heat_capacity(
-    int(input("Enter the mass of water (grammes): ")), 
-    float(input("Enter initial temperature (°C): ")), 
-    float(input("Enter temperature at the end (°C): "))
-)
-
-import math
-
-def volume_cylinder(r, h):
+# Exercise ^18
+def task18():
+    r = int(input("Enter radius of cylinder (rad): "))
+    h = float(input("Enter height of cylinder (cm): "))
     v = math.pi * math.pow(r, 2) * h
     print(f"\nVolume is: {v:.1f} (cm3)")
 
-volume_cylinder(
-    int(input("Enter radius of cylinder (rad): ")),
-    float(input("Enter height of cylinder (cm): "))
-)
-
-import math
-
-def free_fall(h):
+# Exercise ^19
+def task19():
+    h = int(input("Enter please height of object from which you will jump (m): "))
     base_speed = 0
     acceleration = 9.8
     v_free = math.sqrt(math.pow(base_speed, 2) + (2 * acceleration * h))
     print(f"\nFree fall will be: {v_free:.1f} (N)")
 
-free_fall(int(input("Enter please height of object from which you will jump (m): ")))
-
-def ideal_gas(p, v, t):
+# Exercise ^20
+def task20():
 # /// formule: PV = nRT
 # P - pressure (pascales)
 # V - volume(liters)
 # n - amount of thing (mols) 
 # R - const of gas, equal to - 8.314 (joules/(mol*K))
 # T - tmp in Kelvins (K) ///
+    p = float(input("Enter please pressure (pascales): ").replace(" ", ""))
+    v = float(input("Enter please volume (liters): "))
+    t = float(input("Enter please temperature (°C): "))
 
-   pv = p * v
-   t = t + 273.15
+    pv = p * v
+    t = t + 273.15
 #    convert from celsius to kelvin 
-   rt = 8.314 * t
-   n = pv / rt
+    rt = 8.314 * t
+    n = pv / rt
 
-   print(f"\nThe amount of gas (moles) is: {n:.2f}")
+    print(f"\nThe amount of gas (moles) is: {n:.2f}")
 
-ideal_gas(
-    float(input("Enter please pressure (pascales): ").replace(" ", "")),
-    float(input("Enter please volume (liters): ")),
-    float(input("Enter please temperature (°C): "))
-)
-
-def area_triangle(b, h):
+# Exercise ^21
+def task21():
+    b = float(input("Enter a long: "))
+    h = float(input("Enter a height: "))
     area = (b * h) / 2
-    print(f"\nArea will be: {round(area)}\nLong is: {b}\nHeight is: {h}")
+    print(f"\nArea will be: {round(area)} \
+    \nLong is: {b} \
+    \nHeight is: {h}")
 
-area_triangle(float(input("Enter a long: ")), float(input("Enter a height: ")))
-
-
-import math
-
-def area_triangle2(s1, s2, s3):
+# Exercise ^22
+def task22():
+    s1 = float(input("Enter long first part of triangle: "))
+    s2 = float(input("Enter long second part of triangle: "))
+    s3 = float(input("Enter long third part of triangle: "))
     s = sum((s1, s2, s3)) / 2
     area = math.sqrt((s * (s - s1) * (s - s2) * (s - s3)))
     print(f"\nArea will be: {round(area)}")
 
-area_triangle2(
-    float(input("Enter long first part of triangle: ")),
-    float(input("Enter long second part of triangle: ")),
-    float(input("Enter long third part of triangle: "))
-)
-
-import math
-
-def correct_angles(s, n):
+# Exercise ^23
+def task23():
+    s = float(input("Enter a long of side: "))
+    n = int(input("Enter amount of sides: "))
     area = (n * math.pow(s, 2)) / (4 * (math.tan(math.pi / n)))
     print(f"\nArea is: {round(area)} (m2)")
 
-correct_angles(
-    float(input("Enter a long of side: ")),
-    int(input("Enter amount of sides: "))
-)
+# Exercise ^24
+def task24():
+    d = int(input("Enter the amount of days: "))
+    hr = int(input("Enter the hours: "))
+    m = int(input("Enter the minutes: "))
+    s = int(input("Enter the seconds: "))
 
-def time(d, hr, m, s):
     sec_ind = d * 24 * 60 * 60
     sec_inhr = hr * 60 * 60
     sec_inm = m * 60
     summary = sum((sec_ind, sec_inhr, sec_inm, s))
     print(f"\nTotal amount of seconds is: {summary}")
 
-time(
-    int(input("Enter the amount of days: ")),
-    int(input("Enter the hours: ")),
-    int(input("Enter the minutes: ")),
-    int(input("Enter the seconds: "))
-)
+# Exercise ^25
+def task25():
+    d = int(input("Enter the amount of days: "))
+    hr = int(input("Enter the hours: "))
+    m = int(input("Enter the minutes: "))
+    s = int(input("Enter the seconds: "))
 
-def time_updated(d, hr, m, s):
     sec_ind = d * 24 * 60 * 60
     sec_inhr = hr * 60 * 60
     sec_inm = m * 60
@@ -338,24 +324,14 @@ def time_updated(d, hr, m, s):
     sec_string = "%2s" % str(s)
     print(f"{d_sring}:{hr_string}:{m_string}:{sec_string}".replace(" ", "0"))
     
-time_updated(
-    int(input("Enter the amount of days: ")),
-    int(input("Enter the hours: ")),
-    int(input("Enter the minutes: ")),
-    int(input("Enter the seconds: "))
-)
-
-
-import time
-
-def current_time():
+# Exercise ^26
+def task26():
     a = time.asctime()
     print(f"\nCurrent time is: {a}")
-current_time()
 
-import math
-
-def when_easter(year):
+# Exercise ^27
+def task27():
+    year = int(input("Enter the year: "))
     a = year % 19
     b = math.floor(year / 100)
     c = year % 100
@@ -373,61 +349,56 @@ def when_easter(year):
 
     print(f"The month will be: {int(month)}\nThe day will be: {day}")
 
-when_easter(int(input("Enter the year: ")))
-
-import math
-
-def body_mass_index(height, weight):
+# Exercise ^28
+def task28():
 # if users inputs are in inches, fts, pounds
     # bmi_notinCI = (weight / math.pow(height, 2)) * 703
     # print(f"Your body mass index is: {bmi_notinCI}")
+    height = float(input("Enter your height: ")),
+    weight = float(input("Enter your weight: "))
  
     bmi = weight / math.pow(height, 2)
     print(f"Your body mass index is: {bmi:.4f}")
 
-body_mass_index(
-    float(input("Enter your height: ")),
-    float(input("Enter your weight: "))
-)
-
-import math
-
-def temp_wind(ta, v):
+# Exercise ^29
+def task29():
+    ta = float(input("Enter temperature of winds (°С): "))
+    v = float(input("Enter the speed of winds: "))
     wci = 13.12 + 0.6215 * ta - 11.37 * (v ** 0.16) + 0.3965 * (ta * (v ** 0.16))
     print(f"Cofficient is: {math.ceil(wci)}")
     # WCI = 13.12 + 0.6215 * Ta - 11.37 * V0.16 + 0.3965 * Tav0.16
 
-temp_wind(
-    float(input("Enter temperature of winds (°С): ")),
-    float(input("Enter the speed of winds: "))
-)
-
-
-def converter_temp(cels):
+# Exercise ^30
+def task30():
+    cels = float(input("Enter a tempreture in Celsius: "))
     kelvin = cels + 273.15
     fahr = (cels * 9/5) + 32
     print(f"\nIn Fahrenheit: {fahr:.2f}\nIn Kelvin: {kelvin:.2f}")
 
-converter_temp(
-    float(input("Enter a tempreture in Celsius: "))
-)
-
-def pressure_calc(press):
+# Exercise ^31
+def task31():
+    press = float(input("Enter please value of pressure: "))
     psi = press / 6.895
     mercury = press * 7.501
     atmo = press / 101.3 
-    print(f"\nYour output will be: \n{psi:.2f} in PSI, \n{mercury:.2f} in mm of mercury, \n{atmo:.2f} in atmosphere")
+    print(f"\nYour output will be: \
+    \n{psi:.2f} in PSI, \
+    \n{mercury:.2f} in mm of mercury, \
+    \n{atmo:.2f} in atmosphere")
 
-pressure_calc(float(input("Enter please value of pressure: ")))
-
-def nums(n):
+# Exercise ^32
+def task32():
+    n = input("Enter your number: ")
     listed = list(map(int, n))
     summary = sum(listed)
     print(summary)
 
-nums(input("Enter your number: "))
+# Exercise ^33
+def task33():
+    n1 = int(input("Enter a first number: "))
+    n2 = int(input("Enter a second number: "))
+    n3 = int(input("Enter a third number: "))
 
-def triple_int(n1, n2, n3):
     listed = [n1, n2, n3]
     s = sorted(listed)
     print(f"{s[0]}, {s[1]}, {s[2]}")
@@ -437,13 +408,9 @@ def triple_int(n1, n2, n3):
     sum_minused = sum(listed) - mn - mx
     print(f"{mn}, {sum_minused}, {mx}")
 
-triple_int(
-    int(input("Enter a first number: ")),
-    int(input("Enter a second number: ")),
-    int(input("Enter a third number: "))
-)
-
-def yesterday_bread(a):
+# Exercise ^34
+def task34():
+    a = int(input("Enter amount of breads: "))
     base_price = 3.49 * a
     # discount is 60%
     discounted = base_price * 0.6
@@ -452,4 +419,26 @@ def yesterday_bread(a):
           \nDiscount amount: $ {discounted:.2f} \
           \nYour total price for your breads: $ {(total):.2f}")
 
-yesterday_bread(int(input("Enter amount of breads: ")))
+
+def main():
+    while True:
+        choice = input("Enter a number of task (at the moment range is in [1 - 34])\
+        \nif you wanna stop program enter [0]: ")
+        tasks = {
+    '1': task1, '2': task2, '3': task3, '4': task4, '5': task5,
+    '6': task6, '7': task7, '8': task8, '9': task9, '10': task10,
+    '11': task11, '12': task12, '13': task13, '14': task14, '15': task15,
+    '16': task16, '17': task17, '18': task18, '19': task19, '20': task20,
+    '21': task21, '22': task22, '23': task23, '24': task24, '25': task25,
+    '26': task26, '27': task27, '28': task28, '29': task29, '30': task30,
+    '31': task31, '32': task32, '33': task33, '34': task34
+}
+        if choice == "0":
+            print("Program was stopped///...")
+            break
+        elif choice in tasks:
+            tasks[choice]()
+        else:
+            print(f"Your input |{choice}| isn't correct, please enter a number in range [1 - 34]")
+if __name__ == "__main__":
+    main()
