9022959fc315f0f65364566560a2c47803553651
Oleksii Khodus <exalchel@gmail.com>
2025-01-23 20:35:27 +0200

    newfolds

---
+++ javascript/pracs/index.js
@@ -0,0 +1,62 @@
+/*
+task1 - global and functional scopes
+*/
+
+let $new_var = 10
+function GLFN_scopes() {
+    let arr = []
+    for (let i = 0; i < $new_var; i++) {
+        arr.push(i)
+    }
+    console.log(arr)
+    
+}
+GLFN_scopes()
+
+/*
+task2 - block scopes
+*/
+
+function block_scopes(n) {
+    if (n > 0) {
+        let i = 1
+        console.log(i)
+    }
+    else {
+        let i = 0
+        console.log(i)
+    }
+    
+}
+
+block_scopes(3)
+
+/*
+task3 - Lexin scopes
+*/
+
+function l_scopes(n) {
+    if (n > 0){
+        return returned_lscopes()
+    }
+}
+
+function returned_lscopes() {
+    console.log("Oh, n is more than 0")
+}
+
+l_scopes(1)
+
+/*
+task4 - let and var--loop scopes
+*/
+
+function letvar_loop() {
+    for (let i = 2; i < 10; i++) {
+        var $variable = i
+        let $let = i ** 2
+        console.log($variable, $let)   
+    }
+}
+letvar_loop()
+
