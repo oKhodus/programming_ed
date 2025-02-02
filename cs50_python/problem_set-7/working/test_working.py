6ec7588d517da32dec45d0844948a683a08a73ac
Oleksii Khodus <exalchel@gmail.com>
2025-02-03 00:33:42 +0200

    working [task]

--- cs50_python/problem_set-7/working/test_working.py
+++ cs50_python/problem_set-7/working/test_working.py
@@ -1,4 +1,11 @@
 from working import *
 
-def tst():
-    assert convert() == True
+
+def test_conversion():
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("10 AM to 8:50 PM") == "10:00 to 20:50"
+    assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"
+    assert convert("9:60 AM to 5:60 PM") == ValueError
+    assert convert("9 AM - 5 PM") == ValueError
+    assert convert("09:00 AM - 17:00 PM") == ValueError
--- cs50_python/problem_set-7/working/working.py
+++ cs50_python/problem_set-7/working/working.py
@@ -7,11 +7,50 @@ def main():
 
 
 def convert(s):
-    without_minutes = r"[1][0-2]|[1-9]|[1][0-2] AM to [1][0-2]|[0-9] PM"
-    time = re.findall(rf"{without_minutes}", s)
-    # |[1-9]:[0-5][0-9]
+    with_minutes = (
+        r"([1-9]|1[0-2]):([0-5][0-9]) (AM|PM) to ([1-9]|1[0-2]):([0-5][0-9]) (AM|PM)"
+    )
 
-    return time
+    without_minutes = r"([1-9]|1[0-2]) (AM|PM) to ([1-9]|1[0-2]) (AM|PM)"
+
+    mixed_1 = r"([1-9]|1[0-2]) (AM|PM) to ([1-9]|1[0-2]):([0-5][0-9]) (AM|PM)"
+    mixed_2 = r"([1-9]|1[0-2]):([0-5][0-9]) (AM|PM) to ([1-9]|1[0-2]) (AM|PM)"
+
+    time = (
+        re.findall(with_minutes, s)
+        or re.findall(without_minutes, s)
+        or re.findall(mixed_1, s)
+        or re.findall(mixed_2, s)
+    )
+
+    if not time:
+        return ValueError
+
+    if len(time[0]) == 4:
+        from_hour, from_period, to_hour, to_period = time[0]
+        from_min, to_min = "00", "00"
+    elif len(time[0]) == 5:
+        if ":" in s.split(" to ")[0]:
+            from_hour, from_min, from_period, to_hour, to_period = time[0]
+            to_min = "00"
+        else:
+            from_hour, from_period, to_hour, to_min, to_period = time[0]
+            from_min = "00"
+    else:
+        from_hour, from_min, from_period, to_hour, to_min, to_period = time[0]
+
+    def to_24h(hour, minute, period):
+        hour = int(hour)
+        if period == "PM" and hour != 12:
+            hour += 12
+        if period == "AM" and hour == 12:
+            hour = 0
+        return f"{hour:02}:{minute}"
+
+    from_t = to_24h(from_hour, from_min, from_period)
+    to_t = to_24h(to_hour, to_min, to_period)
+
+    return f"{from_t} to {to_t}"
 
 
 if __name__ == "__main__":
