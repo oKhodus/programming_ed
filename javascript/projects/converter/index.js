f55d74d6eaae0ce31933c4f8ac85353283bdfdf4
Oleksii Khodus <exalchel@gmail.com>
2025-06-02 23:18:01 +0300

    working cm to in {'ve maken}

--- javascript/projects/converter/index.js
+++ javascript/projects/converter/index.js
@@ -1,7 +1,18 @@
 function conv() {
     let x1 = document.getElementById("inp1").value;
-    cm_to_inches = Number(x1) / 2.54
-    document.getElementById("out").innerHTML = cm_to_inches;
+    let checkbox1 = document.getElementById("check_cmtoin");
+    cm_to_inches = (Number(x1) / 2.54).toFixed(2);
+
+    out = document.getElementById("out");
+    if (!x1) {
+        out.innerHTML = "Please enter value!"
+    }
+    else if (!checkbox1.checked){
+        out.innerHTML = "Please check the box to continue!"
+    }
+    else{
+        out.innerHTML = `${x1} (cm) will be ${cm_to_inches} (in)`;
+    }
 }
 
 
